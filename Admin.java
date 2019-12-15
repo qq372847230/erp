8199fbbee9c7b4c30e32e8dc43a126db3b0a51d4
qq372847230 <372847230@qq.com>
2019-12-15 16:52:43 +0800

    change Admin.java and Emp.java file

--- Admin.java
+++ Admin.java
@@ -1,4 +1,7 @@
 import java.util.List;
 public class Admin{
 	private List<Emp> emps;
+	private String aid;
+	private List<Action> action;
+	
 }
--- Emp.java
+++ Emp.java
@@ -1,4 +1,7 @@
 public class Emp implements java.io.Serializable {
 	private Integer empno;
+	public String toString(){
+		return "没事干，写着玩";
+	}
 	
 }
