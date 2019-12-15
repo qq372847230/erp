dd7d6bdde8a92f862b8bd092467205f271875a3a
qq372847230 <372847230@qq.com>
2019-12-15 12:47:57 +0800

    change Hello.java in master branch

--- Hello.java
+++ Hello.java
@@ -4,5 +4,7 @@ public class Hello{
 		System.out.println("好好学习，天天向上");
 		System.out.println("世界，你好");
 		System.out.println(new java.util.Date());
+		Admin admin = new Admin();
+		Emp emp = new Emp();
 	}
 }
