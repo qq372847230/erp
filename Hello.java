014c0345e7414e7e6c944e60a9679154c87e0a1b
qq372847230 <372847230@qq.com>
2019-12-15 12:42:34 +0800

    change Hello.java in dev branch

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
+import java.util.*;
 public class Hello{
 	
 	public static void main(String[] args){
-		System.out.println("好好学习，天天向上");
-		System.out.println("世界，你好");
-		System.out.println(new java.util.Date());
+		System.out.println("Hello JAVA");
+		Date date = new Date();
 	}
 }
