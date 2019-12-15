5d2216b11eb926b7ed4ec29e35c118489a3bae3b
qq372847230 <372847230@qq.com>
2019-12-15 13:12:24 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("好好学习，天天向上");
+		System.out.println("好好学习，天天向上");
+		System.out.println("好好学习，天天向上");
 		System.out.println("世界，你好");
 		System.out.println(new java.util.Date());
 		Admin admin = new Admin();
